e3f2fec1a035ef369cea452f6933fb8c7888cd9c
Dystroxic <dystroxic@taw.net>
2020-08-12 10:44:43 -0400

    Fix run flag

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ set -e
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Run the update script
-/bin/bash "$script_dir/update.sh -n"
+/bin/bash "$script_dir/update.sh" -n
 
 # Change to the web console directory
 cd "$script_dir/arma-server-web-admin"
