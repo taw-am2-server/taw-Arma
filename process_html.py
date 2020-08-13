eb728fa60587e289a800f9013b62beade26a45e4
Tirpitz <Loenjaselter@gmail.com>
2020-08-14 00:16:40 +0100

    add html parser.

--- process_html.py
+++ process_html.py
@@ -1,40 +1,37 @@
-import sys
+#!/usr/bin/python3
+"""Parses Arma 3 launcher mod list html
 
+
+"""
 try:
     from BeautifulSoup import BeautifulSoup
 except ImportError:
     from bs4 import BeautifulSoup
-
 import re
-
+import sys
 
 modIDRe = "(\?id=)([0-9]+)"
 def loadMods(file):
+    """
+    take mod html and writes ids to stdout, returns a list of dicts with `name` and `id`
+    :param file: Path to HTML file to load
+    :return: list of dicts of mod names and ids
+    """
     with open(file, "r") as f:
         page = f.read()
-
-    # print(page)
     parsed_html = BeautifulSoup(page, features="html.parser")
-
 
     modlistHTML= (parsed_html.body.findAll('tr', attrs={'data-type':'ModContainer'}))
     modlist = []
     for mod in modlistHTML:
-        print(modlist)
         try:
-            _idStr = mod.find("a",attrs={'data-type':'Link'}).text
-
-            print(modIDRe.find(_idStr))
-
-            # print(_i
-            # print(type(_idStr))
+            _idStr = mod.find("a", attrs={'data-type':'Link'}).text
+            print(re.findall(modIDRe, _idStr)[0][1])
 
             modlist.append({"name": mod.find("td", attrs={"data-type":"DisplayName"}).text, "ID":re.findall(modIDRe, _idStr)[0][1]})
         except:
             pass
-
     return modlist
-
 
 if __name__ =="__main__":
     _modlistFileName = sys.argv [1]
