1a2ea5baddd985fd28663079c18844eacdd9364f
Tirpitz <Loenjaselter@gmail.com>
2020-08-14 16:07:41 +0100

    add battalion selection

--- update.sh
+++ update.sh
@@ -273,7 +273,7 @@ workshop_template_all=$(<$script_dir/workshop_template_all_prefix.html)
 
 
 for modlist in $config_dir/*.html; do
-    modcmd="$base_steam_cmd +workshop_download_item 107410 {mod} validate +quit"
+    modcmd="\"$base_steam_cmd +workshop_download_item 107410 {mod} validate +quit\""
     python3 "$script_dir/process_html.py" "$modlist" | xargs -n 1 -P 1 -I {mod} bash -c  "run_steam_cmd $modcmd  '3' 'downloading mod  {mod}'"
 done
 # Append the workshop template suffix
