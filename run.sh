485fec25feaa48f6f078892686300041ef8ff003
Tirpitz <Loenjaselter@gmail.com>
2020-08-15 22:18:24 +0100

    remove requirement for run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #/bin/bash "$script_dir/update.sh"
 
 # Change to the web console directory
-cd "$script_dir/arma-server-web-admin"
+pushd "$script_dir/arma-server-web-admin"
 
 # Start the web console
 #/usr/bin/node app.js
