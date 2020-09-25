b982409b36fe5d6cb087a4f7afd7f8dcd9cc017c
Tirpitz <Loenjaselter@gmail.com>
2020-09-25 18:56:20 +0100

    add old mod removal

--- update.sh
+++ update.sh
@@ -619,9 +619,6 @@ fi
 pushd "${mod_install_dir}"
 currently_downloaded_mods=($( ls ) )
 pushd $config_dir
-for i in "${currently_downloaded_mods[@]}"; do
-    echo $i
-done
 for i in "${all_mods[@]}"; do
          currently_downloaded_mods=(${currently_downloaded_mods[@]//*$i*})
 done
@@ -630,7 +627,7 @@ for i in "${currently_downloaded_mods[@]}"; do
     rm $i -r
 done
 popd
-#==========================
+#======================
 
 # Add all other requried fields
 panel_config_json=$(echo "$panel_config_json" | jq ".path = \"$arma_dir\" | .port = $web_console_local_port | .prefix = \"$server_prefix\" | .admins = $admin_steam_ids | .parameters |= . + [\"-profiles=$web_console_profiles_dir\", \"-cfg=$basic_cfg_file\"]")
